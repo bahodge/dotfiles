4ba927a70ea52571bd51fd72d95459dfb678fcb0
Ben Hodge <ben.hodge@plusonerobotics.com>
2022-12-13 09:35:24 -0600

    add lazy git

--- scripts/install_base.zsh
+++ scripts/install_base.zsh
@@ -28,6 +28,11 @@ echo "-------- Installing ---------"
 apt install -y wget fzf ripgrep terminator neovim zsh docker-ce docker-ce-cli containerd.io
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# Lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep '"tag_name":' |  sed -E 's/.*"v*([^"]+)".*/\1/')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz -C /usr/local/bin lazygit
+
 echo "------------- Adding user to docker group ---------------"
 
 usermod -aG docker $USER
