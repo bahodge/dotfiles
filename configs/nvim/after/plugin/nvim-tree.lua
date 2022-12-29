ebd4545125bdc49be7614c38fe643c76d6340105
Ben Hodge <ben.hodge@plusonerobotics.com>
2022-12-29 16:38:21 -0600

    remap cd

--- configs/nvim/after/plugin/nvim-tree.lua
+++ configs/nvim/after/plugin/nvim-tree.lua
@@ -12,6 +12,7 @@ require("nvim-tree").setup({
     mappings = {
       list = {
         { key = "u", action = "dir_up" },
+        { key = "cd", action = "cd" },
       },
     },
   },
@@ -19,7 +20,7 @@ require("nvim-tree").setup({
     group_empty = true,
   },
   filters = {
-    dotfiles = true,
+   -- dotfiles = true,
   },
 })
 
