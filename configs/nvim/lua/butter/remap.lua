e569652374b2b4d49725f426bd3f6e5ece966246
Ben Hodge <ben.hodge@plusonerobotics.com>
2023-01-30 11:43:00 -0600

    add global copy paste remaps

--- configs/nvim/lua/butter/remap.lua
+++ configs/nvim/lua/butter/remap.lua
@@ -14,6 +14,17 @@ vim.keymap.set("", "<A-j>", "<C-\\><C-n><C-w>j" )
 vim.keymap.set("", "<A-k>", "<C-\\><C-n><C-w>k" )
 vim.keymap.set("", "<A-l>", "<C-\\><C-n><C-w>l" )
 
+-- Copy & Pasting
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>Y", '"+yg_')
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>yy", '"+yy')
+
+vim.keymap.set("n", "<leader>p", '"+s')
+vim.keymap.set("n", "<leader>P", '"+P')
+vim.keymap.set("v", "<leader>p", '"+p')
+vim.keymap.set("v", "<leader>P", '"+P')
+
 -- Search and replace in file
 vim.keymap.set("v", "<C-r>", "\"hy:%s/<C-r>h//gc<left><left><left>")
 
