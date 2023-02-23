9af89319d32e9919c78b80fa60c0af3124bbe7fe
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2023-02-23 10:21:14 -0600

    Add primeagen's remaps

--- configs/nvim/lua/butter/remap.lua
+++ configs/nvim/lua/butter/remap.lua
@@ -28,4 +28,26 @@ vim.keymap.set("v", "<leader>P", '"+P')
 -- Search and replace in file
 vim.keymap.set("v", "<C-r>", "\"hy:%s/<C-r>h//gc<left><left><left>")
 
+-- Move text that is highlighted
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- Keep cursor at end of line with capital J deletes
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Keep cusor in middle with half page movements
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Keep cursor in middle with searches
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- keep the copied selection in the register
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
