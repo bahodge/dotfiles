5b90ed0d62ab4c3ec802ef407bda0b1276866db3
Ben Hodge <ben.hodge@plusonerobotics.com>
2023-03-02 17:21:37 -0600

    add telescope delete buffer

--- configs/nvim/after/plugin/telescope.lua
+++ configs/nvim/after/plugin/telescope.lua
@@ -1,6 +1,23 @@
-local builtin = require('telescope.builtin')
+local actions = require "telescope.actions"
+local builtin = require 'telescope.builtin'
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+-- vim.keymap.set('n', '<leader>fF', builtin.find_files({ hidden = true }), {})
 vim.keymap.set('n', '<leader>fp', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+-- vim.keymap.set('n', '<leader>fG', builtin.live_grep(), {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+require("telescope").setup {
+  pickers = {
+    buffers = {
+      mappings = {
+        i = {
+          ["<c-d>"] = actions.delete_buffer + actions.move_to_top,
+        }
+      }
+    }
+  }
+}
+
